9f0edb68f72cb073e5ca61f4e3609fb43f232fa5
Manh Hung (Benjamin) Nguyen <hungbenjamin402@gmail.com>
2024-06-23 02:46:33 +0000

    Created Team Page and Routed to app.py

--- app.py
+++ app.py
@@ -109,3 +109,7 @@ def team_nick_val():
 @app.route("/luke_noah_maya")
 def team_luke_noah_maya():
     return render_template('teams/luke_noah_maya.html')
+
+@app.route("/team2")
+def team2:
+    return render_template('teams/team2.html')
